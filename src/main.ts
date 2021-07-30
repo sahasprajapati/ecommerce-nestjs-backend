339829ec601a73e52bdcf49c3e95c4430f11be08
Prajapati, Sahas <b10prajapati@gmail.com>
2021-07-30 14:10:32 +0545

    Added bind address to fastify server listener

--- src/main.ts
+++ src/main.ts
@@ -4,39 +4,42 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import fastifyHelmet from 'fastify-helmet';
 import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new FastifyAdapter({}),
   );
   // const app = await NestFactory.create(AppModule);
   app.enableCors();
-  // app.register(fastifyHelmet, {
-  //   contentSecurityPolicy: {
-  //     directives: {
-  //       defaultSrc: [`'self'`],
-  //       styleSrc: [`'self'`, `'unsafe-inline'`],
-  //       imgSrc: [`'self'`, 'data:', 'validator.swagger.io'],
-  //       scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
-  //     },
-  //   },
-  // });
+  app.register(fastifyHelmet, {
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: [`'self'`],
+        styleSrc: [`'self'`, `'unsafe-inline'`],
+        imgSrc: [`'self'`, 'data:', 'validator.swagger.io'],
+        scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+      },
+    },
+  });
 
   const configService = app.get(ConfigService);
 
   // app.register(fastifyCsrf);
-  // const config = new DocumentBuilder()
-  //   .setTitle('Ecommerce API')
-  //   .setDescription('Ecommerce API description')
-  //   .setVersion('0.1')
-  //   .addTag('Ecommerce')
-  //   .build();
-  // const document = SwaggerModule.createDocument(app, config);
+  const config = new DocumentBuilder()
+    .setTitle('Ecommerce API')
+    .setDescription('Ecommerce API description')
+    .setVersion('0.1')
+    .addTag('Ecommerce')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
 
-  // SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document);
 
-  console.log('PORT', configService.get('PORT'));
-  await app.listen(configService.get('PORT') || 3000);
+  await app.listen(configService.get('PORT') || 3000, '0.0.0.0', () => {
+    console.log('Listening on Port ', configService.get('PORT') || 3000);
+  });
 }
 bootstrap();
