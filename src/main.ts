162e1fc40c20d04da1dc68beffb231f5405ef54a
Prajapati, Sahas <b10prajapati@gmail.com>
2021-07-30 13:57:35 +0545

    Test

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app/app.module';
 async function bootstrap() {
-  // const app = await NestFactory.create<NestFastifyApplication>(
-  //   AppModule,
-  //   new FastifyAdapter(),
-  // );
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  // const app = await NestFactory.create(AppModule);
   app.enableCors();
   // app.register(fastifyHelmet, {
   //   contentSecurityPolicy: {
