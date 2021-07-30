949aa70d735311676e1aaaa6158a63f74355363d
Prajapati, Sahas <b10prajapati@gmail.com>
2021-07-30 13:40:36 +0545

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,33 +14,33 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 @Module({
   imports: [
-    CategoryModule,
+    // CategoryModule,
     ConfigModule.forRoot({
       load: [configuration],
       validate,
       isGlobal: true,
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService<EnvironmentVariables>) => ({
-        type: 'postgres',
-        // url: configService.get('POSTGRES_URL'),
+    // TypeOrmModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   useFactory: (configService: ConfigService<EnvironmentVariables>) => ({
+    //     type: 'postgres',
+    //     // url: configService.get('POSTGRES_URL'),
 
-        host: configService.get('POSTGRES_HOST'),
-        port: configService.get('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USER'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DB'),
-        // entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        autoLoadEntities: true,
-        synchronize:
-          configService.get('NODE_ENV') === 'production' ? false : true,
-      }),
-      inject: [ConfigService],
-    }),
-    AuthModule,
-    UserModule,
-    ProductsModule,
+    //     host: configService.get('POSTGRES_HOST'),
+    //     port: configService.get('POSTGRES_PORT'),
+    //     username: configService.get('POSTGRES_USER'),
+    //     password: configService.get('POSTGRES_PASSWORD'),
+    //     database: configService.get('POSTGRES_DB'),
+    //     // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //     autoLoadEntities: true,
+    //     synchronize:
+    //       configService.get('NODE_ENV') === 'production' ? false : true,
+    //   }),
+    //   inject: [ConfigService],
+    // }),
+    // AuthModule,
+    // UserModule,
+    // ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
